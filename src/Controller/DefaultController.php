13b7a774ac8b45d7ba01d2b1262d5b2028870f2c
Lucas <lucas.corre@ynov.com>
2021-04-13 21:25:59 +0200

    Update 13/04/2021
    newsletter a debug probleme de route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,6 +13,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
+    
     public function index(ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findAll();
--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,33 +2,31 @@
 
 namespace App\Controller;
 
-use App\Entity\Contact;
-use App\Form\ContactType;
+use App\Entity\Newsletter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ContactController extends AbstractController
+class NewsletterController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/newsletters", name="newsletters")
      */
     public function index(Request $request): Response
     {
-        
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsltterType::class, $newsletter);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->addFlash('success', 'Votre message a bien été envoyé');
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($contact);
+            $entityManager->persist($newsletter);
             $entityManager->flush();
         }
 
-        return $this->render('contact.html.twig', [
+        return $this->render('newsletter/index.html.twig', [
             'form' => $form->createView()
         ]);
     }
