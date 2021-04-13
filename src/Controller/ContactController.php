09d63f03b0db522420074118e1019f4b16ddcf8e
Lucas <lucas.corre@ynov.com>
2021-04-13 13:57:53 +0200

    Projet Update

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('contact.html.twig', []);
+        
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($contact);
+        $entityManager->flush();
+        
+
+        return $this->render('contact.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
