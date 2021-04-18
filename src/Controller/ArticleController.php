7d782433b2595ef0b63432f301eee22480914039
Lucas <lucas.corre@ynov.com>
2021-04-18 23:26:27 +0200

    Travaille sur Article
    findall
    find

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article", name="article")
+     * @Route("/article/{id}", name="article")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository, $id): Response
     {
-        $article = $this->getDoctrine()->getRepository(Article::class);
-        dd($article);
+        $article = $articleRepository->find($id);
+        //dd($article);
         return $this->render('article/index.html.twig', [
+            'article' => $article,
             'controller_name' => 'ArticleController',
         ]);
     }
